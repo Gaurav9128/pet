0362975c3cd73bd9f8cd442d881291a5344ba627
Gaurav Jain <jaingaurav212000@gmail.com>
2025-12-25 08:35:06 +0530

    All error

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -14,7 +14,7 @@ import Product from './Pages/Product';
 import PrivacyPolicy from './Pages/PrivacyPolicy.jsx';
 import TermsAndConditions from './Pages/TermCondition.jsx';
 import ContactUs from './Components/ContactUs/ContactUs.jsx';
-import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 
 const App = () => {
