790e0227452f81017a46f757c316958688886340
Gaurav Jain <jaingaurav212000@gmail.com>
2026-04-17 09:44:17 +0530

    new update

--- Front/src/Components/Footer/Footer.jsx
+++ Front/src/Components/Footer/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
               onClick={handleLogoClick}
               style={{
                 cursor: "pointer",
-                height: "130px",  /* Yahan se bhi height control kar sakte hain */
+                height: "140px",  /* Yahan se bhi height control kar sakte hain */
                 width: "auto"
               }}
             />
